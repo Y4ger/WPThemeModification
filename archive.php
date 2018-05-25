240234308ee1dd6893d1e22e4ec84777b32b8ad8
Jacob Yager <jacyager@gmail.com>
2018-05-25 10:28:14 -0400

    Archive - fullwidth and 4/5 content to sidebar size

--- archive.php
+++ archive.php
@@ -12,16 +12,9 @@
 
 get_header(); ?>
 
-<div class="wrap">
+<div class="wrap" id="archive">
 
-	<?php if ( have_posts() ) : ?>
-		<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
-		</header><!-- .page-header -->
-	<?php endif; ?>
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -38,7 +31,7 @@ get_header(); ?>
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
 				get_template_part( 'template-parts/post/content', 'image' );
-				get_template_part( 'template-parts/post/content', 'excerpt' );	
+				get_template_part( 'template-parts/post/content', 'excerpt' );
 
 			endwhile;
 
