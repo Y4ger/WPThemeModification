65575aab8a3cc3b753cd2284540fde8b454e559b
Jacob Yager <jacyager@gmail.com>
2018-05-29 15:53:50 -0400

    Category Specific tags

--- sidebar.php
+++ sidebar.php
@@ -17,7 +17,41 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <aside id="secondary" class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Blog Sidebar', 'twentyseventeen' ); ?>">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<h2 class="widget-title">Add A Feature</h2>
+	<section id="cat_cloud" class="widget widget_tag_cloud">
+		<div class="catcloud">
+			<ul class="cat-selector" role="list">
+				<?php
+				function seoUrl($string) {
+			    //Lower case everything
+			    $string = strtolower($string);
+			    //Make alphanumeric (removes all other characters)
+			    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+			    //Clean up multiple dashes or whitespaces
+			    $string = preg_replace("/[\s-]+/", " ", $string);
+			    //Convert whitespaces and underscore to dash
+			    $string = preg_replace("/[\s_]/", "-", $string);
+		    return $string;
+				}
 
+			 	$thiscat = get_query_var('cat');
+				$mycats = get_categories();
+
+				foreach ($mycats as $cat) {
+					$name = seoUrl($cat->name);
+					if ($name == get_query_var('category_name')){
+						echo "<li class='category-selector selected' id=$name>$cat->name</li>";
+					} else {
+						echo "<li class='category-selector' id=seoUrl$name>$cat->name</li>";
+					}
+				}
+
+				?>
+			</ul>
+		</div>
+	</section>
+
+	<h2 class="widget-title">Details</h2>
 	<?php
 
 				$custom_query = new WP_Query(
